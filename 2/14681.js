4125d9bf7446a890fb0102c49059b0059e39aaa1
xodud3008 <74578453+xodud3008@users.noreply.github.com>
2021-09-14 16:01:23 +0900

    Baekjoon 14681

---
+++ 2/14681.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input.push(line);
+})
+    .on('close', function () {
+        if (input[0] > 0 && input[1] > 0){
+            console.log('1')
+        } else if (input[0] > 0 && input[1] < 0){
+            console.log('4')
+        } else if (input[0] < 0 && input[1] > 0){
+            console.log('2')
+        } else if (input[0] < 0 && input[1] < 0){
+            console.log('3')
+        }
+        process.exit();
+    });
