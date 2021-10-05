995742317102dadc34ab7b3f878e91d389989b4c
xodud3008 <74578453+xodud3008@users.noreply.github.com>
2021-10-05 22:08:20 +0900

    Baekjoon 11654

---
+++ 7/11654.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdin
+});
+
+let input = [];
+
+rl.on('line', function (line){
+    input.push(line);
+
+}).on('close',function (){
+
+    let N = input[0];
+    console.log(N.charCodeAt())
+    process.exit();
+});
+
