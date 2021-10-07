aaa629af080ab0f99a3aabd9c9d0dfdee3771dc8
xodud3008 <74578453+xodud3008@users.noreply.github.com>
2021-10-07 23:16:30 +0900

    baekjoon 10809.js

---
+++ 7/10809.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdin
+});
+
+let input = [];
+
+rl.on('line', function (line){
+    input.push(line);
+
+}).on('close',function (){
+
+    let S = input[0];
+
+    let A = [];
+    let B = [];
+
+    A.push(S.split(''))
+
+    for (let i = 97; i <= 122; i++){
+        B.push(S.indexOf(String.fromCharCode(i)));
+    }
+
+    console.log(B.join(" "));
+
+
+});
+
