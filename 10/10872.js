f74417503ae34c44f248666e56de1a704785c08e
xodud3008 <74578453+xodud3008@users.noreply.github.com>
2021-10-22 22:59:19 +0900

    baekjoon 10872 edit

--- 10/10872.js
+++ 10/10872.js
@@ -3,12 +3,13 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n
 let N = Number(input[0]);
 let answer = 1;
 
+// 0팩토리얼은 1
 if (N === 0){
     console.log(answer)
 } else {
+    // 0이 아닐 때는 해당 숫자까지 늘어나면서 곱하기
     for (let i = 1; i<=N; i++){
         answer = answer * i
     }
     console.log(answer)
 }
-
